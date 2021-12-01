2c2cd9e1e3dfb258cb837d13882582463952a948
ysnalpr <yasinalipour01@gmail.com>
2021-12-01 18:15:08 +0330

    Create post detail api view and test for creating and viewing post

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.http import response
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from blog.models import Post, Category
+
+
+class PostTests(APITestCase):
+    def test_view_posts(self):
+        url = reverse("blog_api:listcreate")
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
