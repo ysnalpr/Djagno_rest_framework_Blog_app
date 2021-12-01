ef11db01e228bf738773fcfbd5ccf32ea2494455
ysnalpr <yasinalipour01@gmail.com>
2021-12-01 20:35:54 +0330

    Write a test for creating an object in drf

--- blog_api/tests.py
+++ blog_api/tests.py
@@ -12,3 +12,18 @@ class PostTests(APITestCase):
         url = reverse("blog_api:listcreate")
         response = self.client.get(url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def create_post(self):
+        self.test_category = Category.objects.create(name="django")
+        self.testuser1 = User.objects.create_user(
+            username="test_user1", password="123456789"
+        )
+        data = {
+            "title": "new",
+            "author": 1,
+            "excerpt": "new",
+            "content": "new",
+        }
+        url = reverse('blog_api:listcreate')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
